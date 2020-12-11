f11b4722927cc6490258b92a4ae68532ffa8d0a6
jeff <jeffhogg86@gmail.com>
2020-12-10 22:19:22 -0800

    adds dotenv

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./model/User');
 require('./services/passport');
+require('dotenv').config();
 
 mongoose.connect(keys.mongoURI, {
   useNewUrlParser: true,
