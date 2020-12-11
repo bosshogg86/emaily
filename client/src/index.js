a23e25566bd0042ea95fd2b524df10af7a4130c9
jeff <jeffhogg86@gmail.com>
2020-12-10 20:49:42 -0800

    fixing versions

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
-import "materialize-css/dist/css/materialize.min.css";
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import reduxThunk from "redux-thunk";
+import 'materialize-css/dist/css/materialize.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
-import App from "./components/App";
-import reducers from "./reducers";
+import App from './components/App';
+import reducers from './reducers';
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
@@ -14,8 +14,8 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.querySelector("#root")
+  document.querySelector('#root')
 );
 
-console.log("Sripe key", process.env.REACT_APP_STRIPE_KEY);
-console.log("Environment is", process.env.NODE_ENV);
+console.log('Stripe key', process.env.REACT_APP_STRIPE_KEY);
+console.log('Environment is', process.env.NODE_ENV);
