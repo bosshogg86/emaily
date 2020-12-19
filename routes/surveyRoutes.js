84618a132291328d750a6ef78ca5b11656e7fa8a
jeff <jeffhogg86@gmail.com>
2020-12-18 23:26:07 -0800

    adds webhook route, path-parser

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
@@ -7,8 +10,46 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for responding!');
+  });
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    const p = new Path('/api/surveys/:surveyId/:choice');
+
+    _.chain(req.body)
+      .map(({ email, url }) => {
+        // Check for url here
+        if (!url) {
+          return res.status(400).json({ error: 'Some error message' });
+        } else {
+          const match = p.test(new URL(url).pathname);
+
+          if (match) {
+            return {
+              email,
+              surveyId: match.surveyId,
+              choice: match.choice,
+            };
+          }
+        }
+      })
+      .compact()
+      .uniqBy('email', 'url', 'surveyId')
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: { $elemMatch: { email: email, responded: false } },
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date(),
+          }
+        ).exec();
+      })
+      .value();
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
