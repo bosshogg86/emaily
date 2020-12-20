b9bf1d28449df54ecd9412ba4852e557b1780142
jeff <jeffhogg86@gmail.com>
2020-12-20 00:37:13 -0800

    edit action type

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -17,7 +17,6 @@ export const submitSurvey = (values, history) => async dispatch => {
   const res = await axios.post('/api/surveys', values);
 
   history.push('/surveys');
-
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
--- index.js
+++ index.js
@@ -7,12 +7,9 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./models/Survey');
 require('./services/passport');
-// require('dotenv').config();
 
-mongoose.connect(keys.mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
@@ -23,24 +20,25 @@ app.use(
     keys: [keys.cookieKey],
   })
 );
-
 app.use(passport.initialize());
 app.use(passport.session());
 
+require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
+
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
+  // like our main.js file, or main.css file!
   app.use(express.static('client/build'));
 
-  // Express will serve up index.html
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
   const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
-
-require('./routes/authRoutes')(app);
-require('./routes/billingRoutes')(app);
-require('./routes/surveyRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
